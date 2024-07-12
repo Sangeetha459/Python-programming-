7a9a267972f5273d6946789436eb68d571c624d8
Sangeetha459 <sangeetharamadeni@gmail.com>
2024-07-12 20:43:34 +0530

    Update Greatest of the two numbers.py

--- Greatest of the two numbers.py
+++ Greatest of the two numbers.py
@@ -3,7 +3,5 @@ b=int(input('b'))
 c=int(input('c'))
 if(a>b):
     print('a value is greatest')
-elif(b>c):
-    print('b value is greatest')
 else:
-    print('c value is greatest')
+    print('b value is greatest')
